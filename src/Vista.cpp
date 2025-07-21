c560fd999571ff368a6cbc47bbb66839577cfc0d
Hilary2112 <hilary.herrera@correounivalle.edu.co>
2025-07-21 11:46:22 -0500

    Cambios en la parte visual del juego

--- src/Vista.cpp
+++ src/Vista.cpp
@@ -30,15 +30,25 @@ void Vista::mostrarTablero(const Tablero& t, const Avatar& a) {
     cout << "   +" << string(10 * 3, '-') << "+" << endl;
 
     for (int i = 0; i < 10; ++i) {
-        cout << (i + 1 < 10 ? " " : "") << i + 1 << " |"; // �ndice de fila
+        cout << (i + 1 < 10 ? " " : "") << i + 1 << " |"; // Índice de fila
         for (int j = 0; j < 10; ++j) {
             if (i == pos.first && j == pos.second) {
-                cout << " A ";
-            } else {
-                cout << " " << t.getValor(i, j) << " ";
+                cout << " ♞ ";
+            }   
+            else if (i == 0 && j == 0){
+                cout << "E";
+            }
+            else if (i==9 && j==9){
+                cout<< "S";
+            }
+            else if (t.getValor(i,j)== 1) {
+                cout << ". ";
+            }
+            else {
+                cout << "█";
             }
         }
-        cout << "|\n";
+        cout << endl;
     }
 
     cout << "   +" << string(10 * 3, '-') << "+" << endl;

d1b3a2573ce6b02cda9d88921420ae7ffb6de3f4
danagomz <danagomez2816@gmail.com>
2025-07-21 11:47:11 -0500

    Agregados comentarios explicativos en métodos

--- include/Tablero.h
+++ include/Tablero.h
@@ -7,16 +7,16 @@ class Tablero
     public:
         Tablero();
         void generarLaberinto(); // llena celdas con 1 y algunos 0
-        void mostrar() const;
+        void mostrar() const; // Muestra el laberinto en consola (const indica que no modifica el objeto)
         bool esValido(int x, int y) const;
         int getValor(int x, int y) const;
 
-        virtual ~Tablero();
+        virtual ~Tablero();   // Destructor virtual (para posible herencia)
 
     protected:
 
     private:
-       int celdas[10][10];
+       int celdas[10][10];        // Matriz 10x10 que almacena el laberinto (1 = camino, 0 = pared)
 
 };
 

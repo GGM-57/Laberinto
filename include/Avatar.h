b841b9945f264533a27877bffea0c3a2f70c6146
danagomz <danagomez2816@gmail.com>
2025-07-21 11:58:35 -0500

    Co

--- include/Avatar.h
+++ include/Avatar.h
@@ -1,27 +1,35 @@
 #ifndef AVATAR_H
 #define AVATAR_H
 #include "Tablero.h"
-#include <utility>
+#include <utility>  // Para usar la plantilla pair
 using namespace std;
 
-class Avatar //representa el soldado
+class Avatar // Clase que representa al personaje que navegará el laberinto
 {
     public:
-        //metodo constructor
-        Avatar(Tablero* t); //recibe un puntero al tablero para que el avatar consulte
-        void colocarAleatoriamente();//colaca al avatar en una posicion aleatoria valida
+        // Constructor que recibe un PUNTERO al tablero
+        // El puntero permite al avatar acceder y modificar el tablero
+        Avatar(Tablero* t);
+        
+        // Coloca al avatar en una posición aleatoria válida (donde haya camino)
+        void colocarAleatoriamente();
+        
+        // Mueve el avatar a una posición adyacente válida
         void mover();
+        
+        // Busca la salida (posición 9,9) usando BFS (Breadth-First Search)
         void buscarSalida();
+        
+        // Devuelve la posición actual como un par de coordenadas (x,y)
+    //Permite almacenar dos valores juntos como un solo objeto. Es como una "pareja" de valores que pueden ser de tipos diferentes.
         pair<int, int> getPosicion() const;
 
+        // Destructor virtual para posible herencia
         virtual ~Avatar();
 
-    protected:
-
     private:
-        int x, y;
-        Tablero* tablero;
-
+        int x, y;           // Coordenadas actuales del avatar
+        Tablero* tablero;   // PUNTERO al tablero donde se mueve el avatar
 };
 
 #endif // AVATAR_H

d482d50fd6b832bd6477cec17be2aace7ed1a625
danagomz <danagomez2816@gmail.com>
2025-07-21 12:04:53 -0500

    comentarios Vista.cpp

--- src/Vista.cpp
+++ src/Vista.cpp
@@ -1,61 +1,65 @@
 #include "Vista.h"
-#include <iostream>
+#include <iostream> 
 using namespace std;
 
+// Implementación del constructor
 Vista::Vista()
 {
-    //ctor
+    // Constructor vacío ya que la clase no necesita inicializar nada
 }
 
+// Implementación del método para mostrar el tablero
 void Vista::mostrarTablero(const Tablero& t, const Avatar& a) {
-    auto pos = a.getPosicion();
+    // Obtiene la posición actual del avatar usando getPosicion()
+    auto pos = a.getPosicion();  // pos es un pair<int,int> con coordenadas (x,y)
 
-    /*
-    for (int i = 0; i < 10; ++i) {
-        for (int j = 0; j < 10; ++j) {
-            if (i == pos.first && j == pos.second)
-                cout << "A ";
-            else
-                cout << t.getValor(i, j) << " ";
-        }
-        cout << endl;
-    }*/
-
-    cout << "    ";
+    // Dibuja encabezado con números de columnas (1-10)
+    cout << "    ";  // Espacios para alinear
     for (int j = 0; j < 10; ++j) {
-        cout << j + 1 << "  ";
+        cout << j + 1 << "  ";  // Números de columna
     }
     cout << endl;
 
+    // Dibuja borde superior del tablero
     cout << "   +" << string(10 * 3, '-') << "+" << endl;
 
+    // Recorre cada fila del tablero
     for (int i = 0; i < 10; ++i) {
-        cout << (i + 1 < 10 ? " " : "") << i + 1 << " |"; // Índice de fila
+        // Muestra número de fila (1-10) con formato
+        cout << (i + 1 < 10 ? " " : "") << i + 1 << " |"; 
+        
+        // Recorre cada columna de la fila actual
         for (int j = 0; j < 10; ++j) {
+            // Si es la posición del avatar
             if (i == pos.first && j == pos.second) {
-                cout << " ♞ ";
+                cout << " ♞ ";  // Símbolo de caballo (avatar)
             }   
+            // Si es la entrada (0,0)
             else if (i == 0 && j == 0){
-                cout << "E";
+                cout << " E ";  // 'E' de Entrada
             }
+            // Si es la salida (9,9)
             else if (i==9 && j==9){
-                cout<< "S";
+                cout << " S ";  // 'S' de Salida
             }
-            else if (t.getValor(i,j)== 1) {
-                cout << ". ";
+            // Si es un camino (valor 1)
+            else if (t.getValor(i,j) == 1) {
+                cout << " . ";  // Punto para camino
             }
+            // Si es una pared (valor 0)
             else {
-                cout << "█";
+                cout << " █ ";  // Bloque sólido para pared
             }
         }
-        cout << endl;
+        cout << "|" << endl;  // Cierra el borde derecho
     }
 
+    // Dibuja borde inferior del tablero
     cout << "   +" << string(10 * 3, '-') << "+" << endl;
 }
 
-
+// Implementación del destructor
 Vista::~Vista()
 {
-    //dtor
+    // No hay recursos que liberar
 }

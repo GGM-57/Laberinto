b1b1a75ccdc65425295f3338651d44205c456955
danagomz <danagomez2816@gmail.com>
2025-07-21 12:07:51 -0500

    comentarios Controlador.h

--- include/Controlador.h
+++ include/Controlador.h
@@ -1,23 +1,24 @@
 #ifndef CONTROLADOR_H
 #define CONTROLADOR_H
-#include "Tablero.h"
-#include "Avatar.h"
-#include "Vista.h"
+
+#include "Tablero.h"  // Para gestionar el laberinto
+#include "Avatar.h"   // Para controlar el personaje
+#include "Vista.h"    // Para mostrar la interfaz visual
 
 class Controlador
 {
     public:
-        Controlador();
+        Controlador();  // Constructor
+        
+        // Método principal que ejecuta el flujo del programa
         void ejecutar();
-        virtual ~Controlador();
-
-    protected:
+        
+        virtual ~Controlador();  // Destructor virtual para posible herencia
 
     private:
-        Tablero tablero;
-        Avatar avatar;
-        Vista vista;
-
+        Tablero tablero;  // Objeto que representa el laberinto
+        Avatar avatar;    // Objeto que representa el personaje
+        Vista vista;      // Objeto que maneja la visualización
 };
 
 #endif // CONTROLADOR_H

8e4318a3dbd50dec2868f8c92fa97e1102cfdb2a
danagomz <danagomez2816@gmail.com>
2025-07-21 12:03:51 -0500

    Comentarios Vista.h

--- include/Vista.h
+++ include/Vista.h
@@ -1,18 +1,23 @@
 #ifndef VISTA_H
 #define VISTA_H
-#include "Tablero.h"
-#include "Avatar.h"
+// Protección contra inclusiones múltiples del archivo de cabecera
+
+#include "Tablero.h"  // Necesario para mostrar el tablero
+#include "Avatar.h"   // Necesario para mostrar la posición del avatar
 
 class Vista
 {
     public:
-        Vista();
+        Vista();  // Constructor básico
+        
+        // Método para mostrar gráficamente el tablero con el avatar
+        // Recibe referencias constantes al tablero y al avatar
         void mostrarTablero(const Tablero& t, const Avatar& a);
-        virtual ~Vista();
-
-    protected:
+        
+        virtual ~Vista();  // Destructor virtual para posible herencia
 
     private:
+        // No tiene atributos privados, solo funciona como interfaz visual
 };
 
 #endif // VISTA_H
